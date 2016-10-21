017648547df05c21ddce92666b184ee4a6641966
Brian Menard <hello@webdevbrian.com>
2016-10-21 19:53:47 +0000

    added title and body columns to post model, implemented basic NEW form (not functional)

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020174424) do
+ActiveRecord::Schema.define(version: 20161021194943) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "body"
   end
 
   create_table "users", force: :cascade do |t|
