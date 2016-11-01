4c5739f941437abbf800fc99710a677e260174db
Brian Menard <hello@webdevbrian.com>
2016-11-01 16:03:41 +0000

    added associations between posts and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessor :username
   
-  
+  has_many :posts
   
 end
