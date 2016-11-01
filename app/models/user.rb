89fff888766febbadf888400de97eb50c2d2287f
Brian Menard <hello@webdevbrian.com>
2016-11-01 19:44:49 +0000

    set up relationships table in database, added associations to relationship model and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   has_many :posts
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
   
 end
