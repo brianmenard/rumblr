3c7f11751fcaa5a1ee6d1f1db5a9740a08beb230
Brian Menard <hello@webdevbrian.com>
2016-11-02 18:11:31 +0000

    added followers view and corresponding action in usersController.  updated readme version.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,8 @@ class UsersController < ApplicationController
        @following = current_user.following 
     end
     
+    def followers
+       @followers = current_user.followers 
+    end
+    
 end
