bd3f70e2947dac1ece353567e8addbb5e82f581a
Brian Menard <hello@webdevbrian.com>
2016-11-08 18:44:06 +0000

    added edit/update/delete actions to postController, updated post partial to include links to those paths

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,6 +43,22 @@ class PostsController < ApplicationController
     def show
        @post = Post.find(params[:id]) 
     end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        redirect_to post_path(@post)
+    end
    
    
 private
